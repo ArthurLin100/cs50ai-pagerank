b4b907c8def5725624187e2080024127665b36e8
Arthur Lin <thur.lin@gmail.com>
2026-03-20 15:46:32 +0900

    implemented iterate page rank

--- pagerank.py
+++ pagerank.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 import sys
+import copy
 
 DAMPING = 0.85
 SAMPLES = 10000
@@ -89,10 +90,9 @@ def sample_pagerank(corpus, damping_factor, n):
     Return a dictionary where keys are page names, and values are
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
-    """
-    print("this is sample_pagerank")
+    """    
 
-    # init return variable
+    # init variable
     samples = {}
     # pick one start
     page = random.choice(list(corpus.keys()))
@@ -103,9 +103,7 @@ def sample_pagerank(corpus, damping_factor, n):
         item = list(probability_distri.keys())
         weights = list(probability_distri.values())
         page = random.choices(item, weights, k=1)[0]  # retrieve new page based on the current page
-        samples[page] = samples.get(page, 0) + 1
-
-    print(samples)
+        samples[page] = samples.get(page, 0) + 1    
 
     total = sum(samples.values())
     samples_rank = {k : v/total for k, v in samples.items()}  # normalization
@@ -121,7 +119,49 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    # init variables
+    page_rank = {k : 1/len(corpus) for k in corpus}    
+    damping_pr = (1 - damping_factor) / len(corpus)
+
+    # pick one start
+    # page = random.choice(list(corpus.keys()))
+
+    while True:
+        
+        current_page_rank = copy.deepcopy(page_rank)
+        #loop through all pages in corpus
+        for page in corpus:
+
+            the_other_factor = 0
+            for the_other in corpus:
+                if the_other == page:
+                    continue
+                if page not in corpus[the_other]:  # doen't have link to this page
+                    if len(corpus[the_other]) == 0: 
+                        # handle page has no links at all. treat it as having links to all pages
+                        the_other_pr = page_rank[the_other]
+                        the_other_num_links = len(corpus)
+                        the_other_factor += the_other_pr / the_other_num_links
+                        continue
+                    else:
+                        continue            
+                the_other_pr = page_rank[the_other]
+                the_other_num_links = len(corpus[the_other])
+                the_other_factor += the_other_pr / the_other_num_links
+            this_pr = damping_pr + damping_factor * the_other_factor
+            current_page_rank[page] = this_pr
+
+        # check if the page ranks change small enough
+        diff = max(
+            abs(current_page_rank[p] - page_rank[p])
+            for p in page_rank
+        )
+        if diff <= 0.001 :
+            break
+        # update the new set of page ranks
+        page_rank = copy.deepcopy(current_page_rank) 
+    return page_rank    
 
 
 if __name__ == "__main__":
